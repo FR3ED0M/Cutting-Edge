90fe7783f3d702a8c4261a1d3ea714b1390c4ad5
L. Garcia <lgarcia2013@fau.edu>
2018-04-06 13:07:33 -0400

    added Project 1
    added the Portfolio - Resume project

---
+++ Pr1 - Portfolio/assets/js/main.js
@@ -0,0 +1,17 @@
+/*======= Skills *=======*/
+jQuery(document).ready(function($) {
+        
+    $('.level-bar-inner').css('width', '0');
+    $(window).on('load', function() {
+        $('.level-bar-inner').each(function() {
+            var itemWidth = $(this).data('level');
+            $(this).animate({
+                width: itemWidth
+            }, 800); 
+        });
+    });
+    
+    //hook to current state to nav bar
+    $('.page-navigation').onePageNav();
+    
+});
